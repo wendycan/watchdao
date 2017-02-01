a316829ea848480a79ccd9ff4c3aab61e940739a
wendycan <wendycaner@gmail.com>
2017-02-01 22:36:47 +0800

    Can control key

---
+++ gokit.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+#include "gokit.h"
+
+// 获取特定开关的状态
+char gokit_keydown(int key, int short_press) {
+    int unsigned long keep_time = 0;
+    if( digitalRead(key) == LOW) {
+        delay(100);
+        if(digitalRead(key) == LOW) {
+            keep_time = gokit_time_s();
+            while(digitalRead(key) == LOW) {
+                if( (gokit_time_s() - keep_time) > short_press * 2) {
+                    return short_press * 2;
+                }
+            }
+            return short_press;
+        }
+        return 0;
+    }
+    return 0;
+}
+
+unsigned long gokit_time_s() {
+    return millis() / 1000;
+}
---
+++ gokit.h
@@ -0,0 +1,14 @@
+#define   PIN0            0
+#define   PIN1            1
+#define   KEY1            6
+#define   KEY2            7
+#define   LED1            13
+#define   KEY1_SHORT_PRESS  1
+// #define   KEY1_LONG_PRESS   2
+#define   KEY2_SHORT_PRESS  4
+// #define   KEY2_LONG_PRESS   8
+#define   NO_KEY            0
+#define   KEY_LONG_TIMER    3   //( 3s )
+
+char gokit_keydown(int key, int short_press);
+unsigned long gokit_time_s();
