728557c96189db47c538548034d248dcbb093f56
wendycan <wendycaner@gmail.com>
2017-02-01 23:06:29 +0800

    Add directories

--- gokit.h
+++ gokit.h
@@ -4,8 +4,8 @@
 #define   KEY2            7
 #define   LED1            13
 #define   KEY1_SHORT_PRESS  1
-// #define   KEY1_LONG_PRESS   2
 #define   KEY2_SHORT_PRESS  4
+// #define   KEY1_LONG_PRESS   2
 // #define   KEY2_LONG_PRESS   8
 #define   NO_KEY            0
 #define   KEY_LONG_TIMER    3   //( 3s )
